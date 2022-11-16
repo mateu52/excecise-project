fe0690db4852aa9247f563a74b5cf7670556898d
mateu52 <wuoelte@gmail.com>
2022-11-16 21:59:43 +0100

    akcje hooki, onClick

--- src/components/calculator/calculator.js
+++ src/components/calculator/calculator.js
@@ -5,38 +5,56 @@ import Col from 'react-bootstrap/Col';
 import './style.css';
 
 function Calculator(){
-    const [ counter, setCounter ] = useState();
+    function useCounter(initialValue){
+        const [ value, setValue ] =useState(initialValue);
+        const sendChange = ( props ) => {
+            setValue(props);
+            console.log(props)
+        };
+        return [ value, sendChange ];
+    }
+
+    const [ counter, setCounter ] = useCounter();//useState({        //x:'34',y:'',z:[2,45,5]    });
+    const onClick = ( props ) => {
+        setCounter(props);
+        console.log(counter)
+    }
+    console.log(counter)
+    //komponent - ekran
     return(
-        <Container className="buttons" >
-            <Row className="row">
-                <Col>7</Col>
-                <Col>8</Col>
-                <Col>9</Col>
-                <Col>DEL</Col>
-            </Row>
-            <Row className="row">
-                <Col>4</Col>
-                <Col>5</Col>
-                <Col>6</Col>
-                <Col>+</Col>
-            </Row>
-            <Row className="row"> 
-                <Col>1</Col>
-                <Col>2</Col>
-                <Col>3</Col>
-                <Col>-</Col>
-            </Row>
-            <Row className="row">
-                <Col>.</Col>
-                <Col>0</Col>
-                <Col>/</Col>
-                <Col>x</Col>
-            </Row>
-            <Row className="row">
-                <Col>RESET</Col>
-                <Col>=</Col>
-            </Row>
+            <div>
+                <h4>napis{counter}</h4>
+            <Container >
+                <Row className="row">
+                    <Col className="col" type="button" onClick={()=>onClick(1)}>7</Col>
+                    <Col className="col" type="button" onClick={()=>onClick(2)}>8</Col>
+                    <Col className="col">9</Col>
+                    <Col className="col">DEL</Col>
+                </Row>
+                <Row className="row">
+                    <Col className="col">4</Col>
+                    <Col className="col">5</Col>
+                    <Col className="col">6</Col>
+                    <Col className="col">+</Col>
+                </Row>
+                <Row className="row"> 
+                    <Col className="col">1</Col>
+                    <Col className="col">2</Col>
+                    <Col className="col">3</Col>
+                    <Col className="col">-</Col>
+                </Row>
+                <Row className="row">
+                    <Col className="col">.</Col>
+                    <Col className="col">0</Col>
+                    <Col className="col">/</Col>
+                    <Col className="col">x</Col>
+                </Row>
+                <Row className="row">
+                    <Col>RESET</Col>
+                    <Col>=</Col>
+                </Row>
         </Container>
+        </div>
     )
 }
 
