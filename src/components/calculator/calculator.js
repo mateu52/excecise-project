831b9eb19766d5c300abc139f4d91ac165e70deb
mateu52 <wuoelte@gmail.com>
2022-11-28 20:03:42 +0100

    calc ok

--- src/components/calculator/calculator.js
+++ src/components/calculator/calculator.js
@@ -39,18 +39,21 @@ function Calculator(){
     //komponent - ekran
     return(
             <div>
-                <h5>{calculation}</h5>
+                {calculation || '0'}
+                <p></p>
+                    {output ? <span className="preRes">{output}</span>:''}
+                
             <Container >
                 <Row className="row">
                     <Col className="col" type="button" value='4' onClick={()=>updateCalculation('2')}>2</Col>
                     <Col className="col" type="button" value='2' onClick={()=>updateCalculation('4')}>4</Col>
-                    <Col className="col" type="button" value='45' onClick={()=>{updateCalculation('+')}}>9</Col>
-                    <Col className="col" type="button" value='45' onClick={clear}>DEL</Col>
+                    <Col className="col" type="button" value='45' onClick={()=>{updateCalculation('+')}}>+</Col>
+                    <Col className="col" type="button" value='45' onClick={clear}> del </Col>
                 </Row>
                 <Row className="row">
-                    <Col className="col" onClick={calculate}>=</Col>
-                    <Col className="col">5</Col>
-                    <Col className="col">6</Col>
+                    <Col className="col" type="button" onClick={calculate}>=</Col>
+                    <Col className="col" type="button" onClick={()=>{updateCalculation('.')}}>.</Col>
+                    <Col className="col" type="button" onClick={()=>{updateCalculation('0')}}>0</Col>
                     <Col className="col">+</Col>
                 </Row>
                 <Row className="row"> 
